12ee674dcc31e14a268491cd0b10040912e653cc
viveketx <109404498+viveketx@users.noreply.github.com>
2022-08-03 20:27:33 +0530

    Update WelcomeController.java

--- src/main/java/com/cdac/api/WelcomeController.java
+++ src/main/java/com/cdac/api/WelcomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class WelcomeController {
+	int count;
 	@GetMapping("/message")
 	public String getMessage() {
 		return "welcome...";
